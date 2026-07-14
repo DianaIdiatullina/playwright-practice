e480bc6242f3db4e8b8d0d0dae813bedd3436113
Diana <id789366@gmail.com>
2026-07-14 16:49:57 +0200

    Add Page Object Model classes and tests

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,32 @@
+import {test, expect} from '@playwright/test'
+import { PageManager } from '../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
+
+
+
+test.beforeEach(async({page}) => {
+    await page.goto('/')
+})
+
+test('navigate to form page @smoke @regression', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
+})
+
+test('parametrized methods @smoke', async({page}) => {
+    const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGrigdFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2')
+    await pm.onFormLayoutsPage().sumbitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
+   
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
+    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
+})
